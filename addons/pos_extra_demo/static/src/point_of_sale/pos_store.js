a014665c913da5eee686dc7f683d5835a0680fd5
parp-odoo <parp@odoo.com>
2026-02-17 18:56:14 +0530

    auto enable dark mode

---
+++ addons/pos_extra_demo/static/src/point_of_sale/pos_store.js
@@ -0,0 +1,9 @@
+import { PosStore } from "@point_of_sale/app/services/pos_store";
+import { patch } from "@web/core/utils/patch";
+
+patch(PosStore.prototype, {
+    async setup() {
+        await super.setup(...arguments);
+        // pass
+    },
+});
